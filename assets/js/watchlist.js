9bb66d28b16fc3b1a2f424a4622c716893ccfd59
BXS-Archipelago <bxs@tutanota.com>
2021-03-14 20:34:24 +0000

    create array and variables list for the Watchlist

---
+++ assets/js/watchlist.js
@@ -0,0 +1,8 @@
+let movieList = []
+
+let currentMovie = ""
+const movieInput = document.querySelector(".movies-input")
+const saveBtn = document.querySelector(".save-btn")
+const ul = document.querySelector(".saved-movies")
+const tick = document.querySelector(".tick")
+const done = document.querySelector(".tick-linethrough")
